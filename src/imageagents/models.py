380e7dbf05d7a08a566251d96b67e8ac6f210d00
JonathanJourney99 <ijonathan.gomes@gmail>
2025-08-04 20:27:35 +0530

    openai

--- src/imageagents/models.py
+++ src/imageagents/models.py
@@ -6,8 +6,11 @@ from dotenv import load_dotenv
 load_dotenv('.env', override=True)
 
 
+# def get_chat_model():
+#     return init_chat_model("anthropic:claude-3-5-sonnet-latest", temperature=0)
+
 def get_chat_model():
-    return init_chat_model("anthropic:claude-3-5-sonnet-latest", temperature=0)
+    return init_chat_model("openai:gpt-4o-mini-2024-07-18", temperature=0)
 
 def get_image_gen_client():
     together_api_key = os.getenv('TOGETHER_API_KEY')
